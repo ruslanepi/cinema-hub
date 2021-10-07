1bd4c5d8de6da528b33ca5ae6f7f14f78b04199f
ruslanepi <ruslanep2@gmail.com>
2021-10-07 21:51:18 +0300

    added saving data in localstorage

--- src/index.js
+++ src/index.js
@@ -10,7 +10,22 @@ import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+// const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
+
+const persistentState = localStorage.getItem('state')
+  ? JSON.parse(localStorage.getItem('state'))
+  : {}
+const store = createStore(
+  rootReducer,
+  persistentState,
+  composeEnhancer(applyMiddleware(thunk))
+)
+
+store.subscribe(() => {
+  const state = store.getState()
+  const serializedState = JSON.stringify(state)
+  localStorage.setItem('state', serializedState)
+})
 
 ReactDOM.render(
   <React.StrictMode>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,11 @@ import libraryReducer from './libraryReducer'
 import detailReducer from './detailReducer'
 import actorsReducer from './actorsReducer'
 
-
 const rootReducer = combineReducers({
   films: filmsReducer,
   library: libraryReducer,
   details: detailReducer,
   actors: actorsReducer,
-  
 })
 
 export default rootReducer
